4711c7f163fd47b5deb9a9814434b1ff0eb36cbc
ValentinCarp <32415675+ValentinKRP@users.noreply.github.com>
2022-05-16 23:44:32 +0300

    Add files via upload

--- Service1/plasare.php
+++ Service1/plasare.php
@@ -1,4 +1,7 @@
 <?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
 function clean($string)
 {
    $string = str_replace(' ', '-', $string);
@@ -14,104 +17,133 @@ $dbname = "ServiceOnline";
 
 $conn = mysqli_connect($servername, $username, $password, $dbname);
 
-var_export($_POST['submit']);
-if (isset($_POST['submit'])) {
+
+
+$id = $_POST['id_user'];
+
+
+
+if (isset($_FILES['filename'])) {
    $imagename = $_FILES["filename"]["name"];
-   echo $imagename;
+   $check = mime_content_type($_FILES["filename"]["tmp_name"]);
+
+   if ($check != 'image/png' && $check != 'image/jpg') {
+      $page = file_get_contents("programare.html");
+      echo $page;
+      echo "<script>alert('Format fisier gresit');</script>";
+      return;
+   }
+
+   $target_dir = "uploads/" . trim($id, "\"");
+
+   if (!file_exists($target_dir)) {
+      mkdir($target_dir, 0777, true);
+   }
+
+
+   // if (move_uploaded_file(basename($_FILES["filename"]["tmp_name"]), $target_dir)) {
+   //    echo "The file has been uploaded.";
+   // } else {
+   //    echo "Sorry, there was an error uploading your file.";
+   //    var_export($_FILES["filename"]);
+   // }
 }
 
-// $id = $_POST['id_user'];
-// $cerere = $_POST['cerere'];
-// $date = $_POST['txtDate'];
-// $time = $_POST['time'];
-// $id = clean($id);
-// if (empty($cerere)) {
-//    $page = file_get_contents("programare.html");
-//    echo $page;
-//    echo "<script>alert('Introduceti cererea');</script>";
-
-//    return;
-// }
-
-// if (empty($date)) {
-//    $page = file_get_contents("programare.html");
-//    echo $page;
-//    echo "<script>alert('Introduceti data');</script>";
-//    return;
-// }
-
-// if (empty($time)) {
-//    $page = file_get_contents("programare.html");
-//    echo $page;
-//    echo "<script>alert('Introduceti ora');</script>";
-//    return;
-// }
-
-
-// $select = "SELECT * from cereri;";
-// $query = mysqli_query($conn, $select);
 
 
 
+$cerere = $_POST['cerere'];
+$date = $_POST['txtDate'];
+$time = $_POST['time'];
+$id = clean($id);
+if (empty($cerere)) {
+   $page = file_get_contents("programare.html");
+   echo $page;
+   echo "<script>alert('Introduceti cererea');</script>";
 
-// $count = mysqli_num_rows($query);
+   return;
+}
 
-// $select2 = "SELECT * from programari";
-// $query2 = mysqli_query($conn, $select2);
+if (empty($date)) {
+   $page = file_get_contents("programare.html");
+   echo $page;
+   echo "<script>alert('Introduceti data');</script>";
+   return;
+}
+
+if (empty($time)) {
+   $page = file_get_contents("programare.html");
+   echo $page;
+   echo "<script>alert('Introduceti ora');</script>";
+   return;
+}
+
+
+$select = "SELECT * from cereri;";
+$query = mysqli_query($conn, $select);
 
 
 
 
-// $ok = 0;
-// $app = "Aprobat";
-// if (mysqli_num_rows($query2) > 0) {
+$count = mysqli_num_rows($query);
 
-
-//    while ($row = mysqli_fetch_assoc($query2)) {
-
-//       if ($row["OraProgramarii"] == $time && $row["DataProgramarii"] == $date) {
-//          $select3 = "SELECT * from cereri where DataDorita='$date' and OraDorita='$time';";
-//          $query3 = mysqli_query($conn, $select3);
-//          if (mysqli_num_rows($query3) > 0) {
-
-
-//             while ($row2 = mysqli_fetch_assoc($query3)) {
-//                if ($row2["StatusCerere"] == $app) {
-//                   $page = file_get_contents("programare.html");
-//                   echo $page;
-//                   echo "<script>alert('Ora rezervata')</script>";
-//                   return;
-//                }
-//             }
-//          }
-//       }
-//    }
-// }
+$select2 = "SELECT * from programari";
+$query2 = mysqli_query($conn, $select2);
 
 
 
-// $count = $count + 1;
-// $id_max = 0;
 
-// if (mysqli_num_rows($query) > 0) {
-
-
-//    while ($row = mysqli_fetch_assoc($query)) {
-
-//       if ($row["ID"] > $id_max)
-//          $id_max = $row["ID"];
-//    }
-// }
-// $stat = "In asteptare";
-// $raspuns = "Niciun raspuns inca";
-// $id_max = $id_max + 1;
+$ok = 0;
+$app = "Aprobat";
+if (mysqli_num_rows($query2) > 0) {
 
 
-// $insert = "INSERT INTO cereri(ID, TextUtilizator, StatusCerere, DataDorita,OraDorita, IdCont,Raspuns) VALUES ('$id_max','$cerere','$stat','$date','$time','$id','$raspuns')";
-// $insert2 = "INSERT INTO programari(ID,DataProgramarii,OraProgramarii,IdUser) VALUES ('$id_max','$date','$time','$id')";
-// $query2 = mysqli_query($conn, $insert);
-// $query3 = mysqli_query($conn, $insert2);
-// mysqli_close($conn);
-// $page = file_get_contents("programare.html");
-// echo $page;
-// echo "<script>alert('Cerere inregistrata cu succes!');</script>";
+   while ($row = mysqli_fetch_assoc($query2)) {
+
+      if ($row["OraProgramarii"] == $time && $row["DataProgramarii"] == $date) {
+         $select3 = "SELECT * from cereri where DataDorita='$date' and OraDorita='$time';";
+         $query3 = mysqli_query($conn, $select3);
+         if (mysqli_num_rows($query3) > 0) {
+
+
+            while ($row2 = mysqli_fetch_assoc($query3)) {
+               if ($row2["StatusCerere"] == $app) {
+                  $page = file_get_contents("programare.html");
+                  echo $page;
+                  echo "<script>alert('Ora rezervata')</script>";
+                  return;
+               }
+            }
+         }
+      }
+   }
+}
+
+
+
+
+$count = $count + 1;
+$id_max = 0;
+
+if (mysqli_num_rows($query) > 0) {
+
+
+   while ($row = mysqli_fetch_assoc($query)) {
+
+      if ($row["ID"] > $id_max)
+         $id_max = $row["ID"];
+   }
+}
+$stat = "In asteptare";
+$raspuns = "Niciun raspuns inca";
+$id_max = $id_max + 1;
+
+
+$insert = "INSERT INTO cereri(ID, TextUtilizator, StatusCerere, DataDorita,OraDorita, IdCont,Raspuns, imagename) VALUES ('$id_max','$cerere','$stat','$date','$time','$id','$raspuns', '$imagename')";
+$insert2 = "INSERT INTO programari(ID,DataProgramarii,OraProgramarii,IdUser) VALUES ('$id_max','$date','$time','$id')";
+$query2 = mysqli_query($conn, $insert);
+$query3 = mysqli_query($conn, $insert2);
+mysqli_close($conn);
+$page = file_get_contents("programare.html");
+echo $page;
+echo "<script>alert('Cerere inregistrata cu succes!');</script>";

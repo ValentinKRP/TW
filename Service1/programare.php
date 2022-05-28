af41493c25313a4a85b60bd208523d562a6522b7
ValentinCarp <32415675+ValentinKRP@users.noreply.github.com>
2022-05-28 13:30:13 +0300

    Add files via upload

--- Service1/programare.php
+++ Service1/programare.php
@@ -75,7 +75,7 @@ include "./models/header.php";
       </p>
     </div>
   </form>
-
+  <script src="./JS/calendar.js"></script>
   <script>
     let monthDates;
 
@@ -110,50 +110,8 @@ include "./models/header.php";
       }
     }
 
-    function checkHours(day, month, year) {
-      let hourDates = monthDates[year + '-' + month + '-' + day];
-      document.getElementById('time').value = 'none';
-
-      for (let i = 8; i <= 18; i++) {
-        document.getElementById('time-' + i).classList.remove('hide-hour');
-      }
-
-      if (hourDates) {
-        console.log(hourDates);
-
-        for (let i = 0; i < hourDates.length; i++) {
-          let currentHour = hourDates[i].split(':')[0];
-          document.getElementById('time-' + currentHour).classList.add('hide-hour');
-        }
-      }
-    }
-
-    function removeClass() {
-      var allElements = document.querySelectorAll("#calendar li");
-      for (i = 0; i < allElements.length; i++) {
-        allElements[i].classList.remove('calendar-selected');
-      }
-    }
 
     checkDates(month, year);
-
-
-    function checkDates(month, year) {
-      const xhttp = new XMLHttpRequest();
-      xhttp.onload = function() {
-        monthDates = JSON.parse(this.responseText);
-        console.log(monthDates);
-        for (const date in monthDates) {
-          // MODIFIED TO 10
-          if (monthDates[date].length > 9) {
-            document.getElementById('li-' + date).classList.add('calendar-full');
-          }
-        }
-
-      }
-      xhttp.open("GET", "checkAvailableDate.php?month=" + month + "&year=" + year, true);
-      xhttp.send();
-    }
   </script>
 
 

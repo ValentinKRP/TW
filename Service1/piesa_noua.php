554ffdb1cdc257534074f21375a07411d25c1b45
Hilote Maria <hilotemg@gmail.com>
2022-05-30 23:51:45 +0300

    Update piesa_noua.php

--- Service1/piesa_noua.php
+++ Service1/piesa_noua.php
@@ -11,43 +11,34 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     
 
     $nume = $_POST["nume"];
-
-    $select = "SELECT * from piese;";
-    $query = mysqli_query($conn, $select);
-
-
-    $ok = 0;
-    $id_max = 0;
-
-    if (mysqli_num_rows($query) > 0) {
-
-        while ($row = mysqli_fetch_assoc($query)) {
-            if ($row["NumePiesa"] == $nume)
-                $ok = 1;
-
-            if ($row["ID"] > $id_max)
-                $id_max = $row["ID"];
-        }
-    }
-
-    $id_max = $id_max + 1;
-    if ($ok == 0) {
-
-        $insert = "INSERT INTO piese(ID, NumePiesa, Numar) VALUES ('$id_max','$nume','$ok')";
-        $query2 = mysqli_query($conn, $insert);
-
-        mysqli_close($conn);
-        echo "<script>
-        alert('Piesa adaugata cu succes!');
-        window.location.href='piesa_noua.php';
-        </script>";
-    } else if ($ok == 1) {
+    
+    $query = "SELECT * FROM piese WHERE NumePiesa ='$nume';";
+    $result = mysqli_query($conn, $query);
+    
+    if (mysqli_num_rows($result) > 0) {
         mysqli_close($conn);
         echo "<script>
         alert('Piesa deja exista!');
         window.location.href='piesa_noua.php';
         </script>";
     }
+    else {
+        $query = "SELECT MAX(ID) from piese;";
+        $result = mysqli_query($conn, $query);
+        $row = mysqli_fetch_row($result);
+        $id_max = $row[0] +1;
+
+        $nr = 0;
+        $query = "INSERT INTO piese(ID, NumePiesa, Numar) VALUES ('$id_max','$nume','$nr')";
+        $insert = mysqli_query($conn, $query);
+    
+        mysqli_close($conn);
+        echo "<script>
+        alert('Piesa adaugata cu succes!');
+        window.location.href='piesa_noua.php';
+        </script>";
+    }
+
 }
 
 
@@ -66,8 +57,10 @@ include "./models/header.php";
     <?php include_once "./models/navbar.php" ?>
 
     <h3 style="text-align:center">
-        <img alt="?" src="image/logo.png" style="border-radius: 50%;" width="160" height="160">
-    </h3>
+      <a>Adaugare Piesa Noua</a>
+      <br>
+      <img alt="?" src="image/logo.png" style="border-radius: 50%;" width="160" height="160">
+   </h3>
 
     <form method="post" action="piesa_noua.php">
         <div style="text-align:center" class="container">
